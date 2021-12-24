30d1436cb5cc3876a5bb979bddfdfa8db89aca4e
Samuel Wilson <samWson@users.noreply.github.com>
2021-12-24 20:29:34 +1300

    Extension can retrieve base-command from configuration options

--- credo-nova.novaextension/Scripts/main.js
+++ credo-nova.novaextension/Scripts/main.js
@@ -7,10 +7,14 @@ exports.deactivate = function() {
     // Clean up state before the extension is deactivated
 }
 
+/** Provides the main interface for invoking Credo from within Nova. */
+class CredoProcess {
+
+}
 
 class Credo {
     constructor() {
-
+        this.baseCommand = this.getConfig("samuel-wilson.credo-nova.base-command");
     }
 
     provideIssues(editor) {
@@ -28,8 +32,20 @@ class Credo {
 
         return issues;
     }
+
+    /**
+     * Fetches user preferences from Nova, with workspace preferences taking
+     * priority over general extension preferences.
+     * @param {String} name - The name of the configuration setting to retrieve, as labelled in ./configuration-options.json.
+     * @return {(String | Boolean)} - The value of the user preference from Nova.
+     */
+    getConfig(name) {
+        const workspaceConfig = nova.workspace.config.get(name) ? nova.workspace.config.get(name) : null;
+        const extensionConfig = nova.config.get(name);
+
+        return workspaceConfig === null ? extensionConfig : workspaceConfig;
+    }
 }
 
-
-nova.assistants.registerIssueAssistant("elixir", new Credo());
+nova.assistants.registerIssueAssistant("elixir", new Credo(), { event: "on-save" });
 
